0476172f2a42c8c4ded59f21fa6c327e01cc184b
veer <veer88virk@gmail.com>
2020-09-01 11:00:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import Home from "./component/Home";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <BrowserRouter>
+            <React.Fragment>
+                <Switch>
+                    <Route exact path={'/'} render={() => {
+                        return <Redirect to={'/launches'}/>
+                    }}/>
+                    <Route exact path={'/launches'} component={Home}/>
+                </Switch>
+            </React.Fragment>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/component/FilterList.js
+++ src/component/FilterList.js
@@ -3,22 +3,25 @@ import axios from 'axios';
 import '../App.css';
 
 const buttons = [
-  { name: "All", value: "All" },
-  { name: "2006", value: "2006" },
-  { name: "2007", value: "2007" },
-  { name: "2008", value: "2008" },
-  { name: "2009", value: "2009" },
-  { name: "2010", value: "2010" },
-  { name: "2011", value: "2011" },
-  { name: "2012", value: "2012" },
-  { name: "2013", value: "2013" },
-  { name: "2014", value: "2014" },
-  { name: "2015", value: "2015" },
-  { name: "2016", value: "2016" },
-  { name: "2017", value: "2017" },
-  { name: "2018", value: "2018" },
-  { name: "2019", value: "2019" },
-  { name: "2020", value: "2020" }
+  { name: "2006" },
+  { name: "2007" },
+  { name: "2008" },
+  { name: "2009" },
+  { name: "2010" },
+  { name: "2011" },
+  { name: "2012" },
+  { name: "2013" },
+  { name: "2014" },
+  { name: "2015" },
+  { name: "2016" },
+  { name: "2017" },
+  { name: "2018" },
+  { name: "2019" },
+  { name: "2020" }
+];
+const buttons1 = [
+  { name: "true"},
+  { name: "false" }
 ];
 class FilterList extends Component{
 	state = {
@@ -26,7 +29,7 @@ class FilterList extends Component{
     filterlaunches: []
   }
 	componentDidMount(){
-		axios.get('https://api.spacexdata.com/v3/launches?100')
+		axios.get('https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true')
 		  .then(res => {
 		  	const launches = res.data;
         	this.setState({ launches });
@@ -38,59 +41,75 @@ class FilterList extends Component{
 	}
 	handleClick = name => {
     let filterlaunches = [];
-    if (name === "All") {
-      filterlaunches = this.state.launches;
-    } else {
+    if (name === "true") {
+      	filterlaunches = this.state.launches.filter(
+        launches => launches.launch_success === true);
+    }
+    else if (name === "false") {
+      	filterlaunches = this.state.launches.filter(
+        launches => launches.launch_success === false);
+    }
+    else{
       filterlaunches = this.state.launches.filter(
         launches => launches.launch_year === name
       );
     }
-
     this.setState({ filterlaunches });
   };
+
 	render(){
 		return(
-			<div className="main-container">
+			<div>
 				<h2 className="mb5">SpaceX Launch Programs</h2>
 		    	<section>
 		        <div>
-		          <div className="d-flex flex-wrap">
+		          <div className="d-flex">
 		            <div className="filterPNL">
 		              <div className="sidebar-block">
 		                <h3 className="sidebar-heading">Filters</h3>
 		                <div className="fliterHead"><span>Launch Year</span></div>
 		                <div className="sideBar-buttons d-flex flex-wrap justify-content-between">
-		                  {buttons.map(({ name, value }) => (
+		                  {buttons.map(({ name }) => (
 	                        <button
 	                          key={name}
-	                          value={value}
 	                          onClick={this.handleClick.bind(this, name)}
 	                        >
 	                          {name}
 	                        </button>
 	                      ))}
 		                </div>
+
+		                <div className="fliterHead"><span>Successfull Launch</span></div>
+		                <div className="sideBar-buttons d-flex flex-wrap justify-content-between">
+		                  {buttons1.map(({ name }) => (
+	                        <button
+	                          key={name}
+	                          onClick={this.handleClick.bind(this, name)}
+	                        >
+	                          {name}
+	                        </button>
+	                      ))}
+		                </div>
+		                
 		              </div>
 		            </div>
 		            <div className="productRPNL">
 		              <div className="row d-flex flex-wrap">
-
 		              	{this.state.filterlaunches.map(launches => 
 		              		<div className="launchPNL">
 		              		<img src={launches.links.mission_patch} className="launch_img" alt="img"/>
-		              		<div>{launches.mission_name} #{launches.flight_number}</div>
-		              		<div>Mission_ids:
+		              		<div className="mission_name">{launches.mission_name} #{launches.flight_number}</div>
+		              		<div className="mb5">Mission_ids:
 		              			<ul>
 		              				<li>{launches.mission_id}</li>
 		              			</ul>
 		              		</div>
-		              		<div>Launch Year: {launches.launch_year}</div>
-		              		<div>Successfull Launch: {launches.launch_success}</div>
+		              		<div className="mb5">Launch Year: {launches.launch_year}</div>
+		              		<div className="mb5">Successfull Launch: {launches.launch_success}</div>
 		              		<div>Successfull Landing: {launches.launch_success}</div>
 		              		</div>
 		              	)}
-		              
-		                     
+		              	                     
 		              </div>  
 		            </div>  
 		          </div>
