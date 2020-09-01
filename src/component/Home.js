b81011b0077ef97bf5279d9a10b6c12b53e1a959
veer <veer88virk@gmail.com>
2020-09-01 11:01:01 +0530

    changes

---
+++ src/component/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FilterList from "./FilterList";
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <div className="main-container">
+               <FilterList/>
+            </div>
+        </React.Fragment>
+    );
+};
+
+
+export default Home;
